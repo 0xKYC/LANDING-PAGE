6a9842e546ae903bfc8cd2ad80a0b734cb6b2269
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2023-01-19 15:00:20 +0100

    fix: disable dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import 'swiper/css/autoplay';
 import { AppProps } from 'next/dist/shared/lib/router/router';
 
 import Head from 'next/head';
-import { ColorModeScript } from 'nextjs-color-mode';
+// import { ColorModeScript } from 'nextjs-color-mode';
 import React, { PropsWithChildren } from 'react';
 
 import Footer from 'components/Footer';
@@ -64,7 +64,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         /> */}
         {/* <script async src="https://www.google-analytics.com/analytics.js"></script> */}
       </Head>
-      <ColorModeScript />
+      {/* <ColorModeScript /> */}
       <GlobalStyle />
 
       <Providers>
