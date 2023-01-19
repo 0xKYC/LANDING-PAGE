274148cc788340be746b45b169f9496b9ee38142
ankerx <93661548+ankerx@users.noreply.github.com>
2023-01-19 18:21:05 +0100

    change privacy policy format

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -9,13 +9,26 @@ export default function ContactPage() {
         <RichText>
           <p>
             At 0xKYC, Inc., we take the privacy of our website users seriously. This policy outlines the information we collect, how it is
-            used, and the steps we take to protect it. Information Collection: We collect personal information such as name, email address,
-            and phone number when you voluntarily provide it to us through forms on our website. Use of Information: We use the information
-            collected to contact you regarding updates about our services and to respond to any inquiries you may have. Sharing of
-            Information: We do not share or sell any personal information to any third parties. Security Measures: We take appropriate
-            measures to protect the personal information submitted to us, both during transmission and once we receive it. If you have any
-            questions or concerns about our Privacy Policy or the information we collect, please contact us. This Privacy Policy is
-            effective as of the date of posting and may be updated from time to time.
+            used, and the steps we take to protect it.
+            <Text>Information Collection: </Text>
+            We collect personal information such as name, email address, and phone number when you voluntarily provide it to us through
+            forms on our website.
+            <Text>Use of Information: </Text>
+            <p>
+              We use the information collected to contact you regarding updates about our services and to respond to any inquiries you may
+              have.
+            </p>
+            <Text>Sharing of Information: </Text>
+            <p>We do not share or sell any personal information to any third parties. </p>
+            <Text>Security Measures: </Text>
+            <p>
+              We take appropriate measures to protect the personal information submitted to us, both during transmission and once we receive
+              it.
+            </p>
+            <Text>
+              If you have any questions or concerns about our Privacy Policy or the information we collect, please contact us. This Privacy
+              Policy is effective as of the date of posting and may be updated from time to time.
+            </Text>
           </p>
         </RichText>
       </PrivacyPolicyContainer>
@@ -27,4 +40,7 @@ const PrivacyPolicyContainer = styled.div`
   max-width: 90rem;
   margin: auto;
   overflow-x: auto;
+`;
+const Text = styled.p`
+  margin-top: 1rem;
 `;
